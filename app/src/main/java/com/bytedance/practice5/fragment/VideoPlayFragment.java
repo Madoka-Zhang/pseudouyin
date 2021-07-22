85b676243a9081561bb21b3fa2d51c5954bdaa08
sin314259cos <763493572@qq.com>
2021-07-22 19:47:05 +0800

    重新加了点东西

--- app/src/main/java/com/bytedance/practice5/fragment/VideoPlayFragment.java
+++ app/src/main/java/com/bytedance/practice5/fragment/VideoPlayFragment.java
@@ -1,12 +1,17 @@
 package com.bytedance.practice5.fragment;
 
+import android.content.Context;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -14,8 +19,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bytedance.practice5.Love;
 import com.bytedance.practice5.R;
 
+
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.zip.Inflater;
 
 public class VideoPlayFragment extends Fragment {
@@ -28,14 +37,20 @@ public class VideoPlayFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_video_play, container, false);
-        mockUrl= getActivity().getIntent().getStringExtra("1");
+        mockUrl = getActivity().getIntent().getStringExtra("1");
         Log.d(TAG, mockUrl);
         VideoView videoView = view.findViewById(R.id.vv_detail);
         videoView.setVideoURI(Uri.parse(mockUrl));
         videoView.setMediaController(new MediaController(getActivity()));
         videoView.start();
-        return  view;
+        Love love = view.findViewById(R.id.love);
+//        if (getContext()!=null) {
+//            WindowManager mWm = (WindowManager) getContext().getApplicationContext().getSystemService(
+//                    Context.WINDOW_SERVICE);
+//            WindowManager.LayoutParams mWmParams = new WindowManager.LayoutParams();
+//            mWm.addView(love, mWmParams);
+//        }
+        return view;
 
-
-    };
+    }
 }
